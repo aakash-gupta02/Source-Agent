e703e556d722a92e7ead9a071c3b43b6d3f3bad7
aakash-gupta02 <editsky597@gmail.com>
2026-08-28 18:50:02 +0530

    implement basic LG agent

--- src/modules/sql/sql.ts
+++ src/modules/sql/sql.ts
@@ -1,20 +1,62 @@
 import { model } from "../../core/config/model.js";
 import { executeSQL, getSchema } from "./db.service.js";
 
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+
+const tools = new ToolNode([getSchema, executeSQL]);
+
 const modelWithTools = model.bindTools([getSchema, executeSQL]);
 
-const response = await modelWithTools.invoke(
-  "Which user has spent the most money on completed orders?",
-);
+import { BaseMessage, HumanMessage } from "@langchain/core/messages";
+import { Annotation, END, START, StateGraph } from "@langchain/langgraph";
 
-console.dir(response, { depth: null });
+const State = Annotation.Root({
+  messages: Annotation<BaseMessage[]>({
+    reducer: (left, right) => left.concat(right),
+    default: () => [],
+  }),
+});
 
-if (!response.tool_calls || response.tool_calls.length === 0) {
-  throw new Error("No tool calls found in the response.");
-}
+const agent = async (state: typeof State.State) => {
+  const response = await modelWithTools.invoke(state.messages);
 
-const toolCall = response.tool_calls[0];
+  return {
+    messages: [response],
+  };
+};
 
-const toolResult = await getSchema.invoke(toolCall.args);
+const shouldContinue = (state: typeof State.State) => {
+  const lastMessage = state.messages.at(-1);
 
-console.dir(toolResult, { depth: null });
+  if (
+    lastMessage &&
+    "tool_calls" in lastMessage &&
+    lastMessage.tool_calls?.length
+  ) {
+    return "tools";
+  }
+
+  return END;
+};
+
+const graph = new StateGraph(State)
+  .addNode("agent", agent)
+  .addNode("tools", tools)
+
+  .addEdge(START, "agent")
+
+  .addConditionalEdges("agent", shouldContinue, ["tools", END])
+
+  .addEdge("tools", "agent")
+
+  .compile();
+
+const result = await graph.invoke({
+  messages: [
+    new HumanMessage(
+      "Which user has spent the most money on completed orders?",
+    ),
+  ],
+});
+
+console.dir(result.messages, { depth: null });
