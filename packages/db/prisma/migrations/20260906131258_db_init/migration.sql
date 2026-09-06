235eb0004338dbe8d4a39c62e21bbd23f59c8003
aakash-gupta02 <editsky597@gmail.com>
2026-09-06 19:15:13 +0530

    add user schema & migration

---
+++ packages/db/prisma/migrations/20260906131258_db_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
+
+-- CreateEnum
+CREATE TYPE "AuthProvider" AS ENUM ('LOCAL', 'GOOGLE', 'GITHUB');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "provider" "AuthProvider" NOT NULL,
+    "providerId" TEXT,
+    "password" TEXT,
+    "refreshToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_providerId_key" ON "User"("providerId");
