8126e4fd7003c5e1f1f0a32cd8312f81c42493ba
aakash-gupta02 <editsky597@gmail.com>
2026-09-08 22:27:14 +0530

    feat(encryption): fix encyption & decryption by adding auth tag

--- apps/backend/src/shared/utils/encryption/encryption.ts
+++ apps/backend/src/shared/utils/encryption/encryption.ts
@@ -12,22 +12,29 @@ export function encrypt(text: string, version: string) {
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
 
-  return iv.toString("hex") + ":" + encrypted;
+  const authTag = cipher.getAuthTag().toString("hex");
+
+  return `${iv.toString("hex")}:${encrypted}:${authTag}`;
 }
 
 export function decrypt(encryptedText: string, version: string) {
   const key = getKey(version);
 
-  const [ivHex, encrypted] = encryptedText.split(":");
+  const [ivHex, encrypted, authTagHex] = encryptedText.split(":");
+
+  if (!ivHex || !encrypted || !authTagHex) {
+    throw new Error("Invalid encrypted data.");
+  }
+
   const iv = Buffer.from(ivHex, "hex");
+  const authTag = Buffer.from(authTagHex, "hex");
 
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  decipher.setAuthTag(authTag);
 
   let decrypted = decipher.update(encrypted, "hex", "utf8");
   decrypted += decipher.final("utf8");
 
   return decrypted;
 }
-
-// const decryptedData = decrypt("2303c1b905057a260488c86237e28f13:0f35299fe2e51d76153febcf8c9fa53f1cf5eb7c54c1d28801b7e396f5505302680544f38f4ce83e9671ec74742a7163c2ff138d309aaf4fd873f4b9ac131b0381", "v1");
-// console.log(decryptedData);
