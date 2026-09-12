f030a688b90b9261d148683492716dc0c974f061
aakash-gupta02 <editsky597@gmail.com>
2026-09-12 15:54:36 +0530

    refactor(agent): update package configuration and add new index module exports

---
+++ packages/agent/src/index.ts
@@ -0,0 +1,3 @@
+export { createModel } from "./core/llm/model.factory.js";
+export { createPostgresConnection } from "./core/database/postgres.connection.js";
+export { createSqlAgent } from "./modules/sql/sql.js";
